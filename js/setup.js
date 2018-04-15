19ee3c704d6eafa113155d75e408770e3c8dc19c
Vitaly Bryukhovetsky <bruhovet@list.ru>
2018-04-15 22:34:08 +0300

    nodule4-task1

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var QUANTITY_SIMILAR_WIZARDS = 4;
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
 var choicesList = {
   firstNames: [
     'Иван',
@@ -78,10 +80,71 @@ var getTemplateClone = function (template, wizard) {
   return clone;
 };
 
-// Разблокируем показ слоя класса «overlay setup»
+// ////////////////////////////////
+// Скрытие и показ окна настроек //
+// ////////////////////////////////
+// Закрытие popup-а по клику на элементе
+var onCloseTriggerClick = function () {
+  overlaySetup.classList.add('hidden');
+  removeListenersForClose();
+};
+// Закрытие popup-а клавишей ESC
+var onDocumentEscKeydown = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    overlaySetup.classList.add('hidden');
+    removeListenersForClose();
+  }
+};
+// Зкрытие popup-а нажатием ввод на элементе
+var onCloseTriggerEnterKeydown = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    overlaySetup.classList.add('hidden');
+    removeListenersForClose();
+  }
+};
+// Предотвращает закрытие popup-а по ESC если элемент выбран
+var onNameInputEscKeydown = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    evt.stopPropagation();
+  }
+};
+// Функция добавляет слушатели на popup настроек
+var addListenersForClose = function () {
+  closeSetupTrigger.addEventListener('click', onCloseTriggerClick);
+  closeSetupTrigger.addEventListener('keydown', onCloseTriggerEnterKeydown);
+  document.addEventListener('keydown', onDocumentEscKeydown);
+  inputName.addEventListener('keydown', onNameInputEscKeydown);
+};
+// Функция удаляет слушатели с popup-а настроек
+var removeListenersForClose = function () {
+  closeSetupTrigger.removeEventListener('click', onCloseTriggerClick);
+  closeSetupTrigger.removeEventListener('keydown', onCloseTriggerEnterKeydown);
+  document.removeEventListener('keydown', onDocumentEscKeydown);
+  inputName.removeEventListener('keydown', onNameInputEscKeydown);
+};
+// popup настроек
 var overlaySetup = document.querySelector('.setup');
-overlaySetup.classList.remove('hidden');
+// Триггер открытия popup-а
+var openSetupTrigger = document.querySelector('.setup-open');
+// Триггер скрытия popup-а
+var closeSetupTrigger = overlaySetup.querySelector('.setup-close');
+// Поле ввода имени персонажа
+var inputName = overlaySetup.querySelector('#username');
 
+openSetupTrigger.addEventListener('click', function () {
+  overlaySetup.classList.remove('hidden');
+  addListenersForClose();
+});
+openSetupTrigger.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    overlaySetup.classList.remove('hidden');
+    addListenersForClose();
+  }
+});
+
+// ////////////////////////////////////
+// Отображение «похожих волшебников» //
+// ////////////////////////////////////
 // Получаем шаблон
 var template = document.querySelector('#similar-wizard-template')
     .content.querySelector('div');
@@ -91,8 +154,64 @@ var similarWizards = getSimilarWizards(QUANTITY_SIMILAR_WIZARDS);
 for (var i = 0; i < similarWizards.length; i++) {
   fragment.appendChild(getTemplateClone(template, similarWizards[i]));
 }
-
 // Добавляем полученный блок «fragment» в ДОМ
 document.querySelector('.setup-similar-list').appendChild(fragment);
 // Отменяем скрытие блока «setup-similar»
 overlaySetup.querySelector('.setup-similar').classList.remove('hidden');
+
+// ///////////////////////////////////
+// Настройтка цвета глаз и фаербола //
+// ///////////////////////////////////
+var QUANTITY_BASE_COLOR = 3;
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+// Возвращает следующий за текущим элемент из массива (массив зациклин)
+var getNextColor = function (colorList, currentColor) {
+  var nextIndex = 1;
+  if (currentColor) {
+    nextIndex = (colorList.indexOf(currentColor) + 1) % colorList.length;
+  }
+  return colorList[nextIndex];
+};
+// Возвращает цвет в шестнадцатиричном формате из rgb
+var convertRgbToHex = function (rgb) {
+  rgb = rgb.match(/(\d+)/g);
+  var hexColor = null;
+  if (rgb) {
+    hexColor = '#';
+    // var убран по тербованию теревиса
+    for (i = 0; i < QUANTITY_BASE_COLOR; i++) {
+      hexColor += parseInt(rgb[i], 10).toString(16);
+    }
+  }
+  return hexColor;
+};
+
+// Получаем контейнер содержащий настройки персонажа
+var setupPlayerElement = document.querySelector('.setup-player');
+// Находим элемент «глаза» персонажа
+var eyesColorInputElement = setupPlayerElement.querySelector('#eyes-color');
+// Вешаем слушатель на глаза
+setupPlayerElement.querySelector('.wizard-eyes').addEventListener(
+    'click',
+    function (evt) {
+      // Меняем цвет глаз
+      evt.target.style.fill = getNextColor(eyesColors, evt.target.style.fill);
+      // Передаем новый цвет глаз в поле input
+      eyesColorInputElement.value = evt.target.style.fill;
+    }
+);
+
+// Находим элемент fireball
+var fireballColorInput = setupPlayerElement.querySelector('#fireball-color');
+// Вешаем на fireball слушатель
+setupPlayerElement.querySelector('.setup-fireball').addEventListener(
+    'click',
+    function (evt) {
+      // Передаем новый цвет в поле input
+      fireballColorInput.value = getNextColor(fireballColors,
+          convertRgbToHex(evt.target.style.backgroundColor));
+      // Меняем цвет fireball-а
+      evt.target.style.backgroundColor = fireballColorInput.value;
+    }
+);
